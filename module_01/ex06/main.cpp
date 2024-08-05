855543111f3afadccc538bc36c0cbb4322ad4754
tcampbel <tcampbel22@gmail.com>
2024-08-05 18:16:48 +0300

    added ex06 directories and files

--- module_01/ex06/main.cpp
+++ module_01/ex06/main.cpp
@@ -1,31 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   harl.hpp                                           :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tcampbel <tcampbel@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/05 14:18:19 by tcampbel          #+#    #+#             */
-/*   Updated: 2024/08/05 18:00:20 by tcampbel         ###   ########.fr       */
+/*   Created: 2024/08/05 18:15:14 by tcampbel          #+#    #+#             */
+/*   Updated: 2024/08/05 18:15:17 by tcampbel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef HARL_HPP
-#define HARL_HPP
-
-#include <iostream>
-
-class Harl
-{
-private:
-	void		debug(void);
-	void		info(void);
-	void		warning(void);
-	void		error(void);
-	void 		(Harl::*complainPtr[4])();
-public:
-	Harl();
-	~Harl();
-	void		complain(std::string level);
-};
-#endif
